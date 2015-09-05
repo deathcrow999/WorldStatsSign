4943ef8865fa575e9a1f1d127e5af9cdbfb08b71
deathcrow999 <deathcrow4561@gmail.com>
2015-09-05 17:01:53 -0400

    Added skeleton

--- src/WSS/WorldStatsSign.php
+++ src/WSS/WorldStatsSign.php
@@ -1,5 +1,28 @@
 <?php
+ 
+namespace WorldStatsSign;
+ 
+use pocketmine\plugin\PluginBase;
+
+ 
+class Main extends PluginBase{
+ 
+        public function onLoad(){
+                $this->getLogger()->info("WorldStatsSign has been loaded");
+        }
+ 
+        public function onEnable(){
+                $this->getLogger()->info("WorldStatsSign has been enabled");
+                $this->getServer()->getPluginManager()->registerEvents($this,$this);
+        }
+ 
+                public function onDisable() {
+                                $this->getLogger()->info("WorldStatsSign has been disabled");
+        }
 
 
+
+
+}
 
 ?>
